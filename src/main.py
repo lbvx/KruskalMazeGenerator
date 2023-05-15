75b9386ba8b57ef56af2b9ea4488391389ad9b51
Guilherme Rogelin Vial <78435405+GRVial@users.noreply.github.com>
2023-05-15 09:13:52 -0300

    criando menu

--- src/main.py
+++ src/main.py
@@ -1,15 +1,42 @@
 from Maze import Maze
 from MazeImage import maze_to_image
 
-def main():
-    rows = int(input('linhas: '))
-    columns = int(input('colunas: '))
-    seed = input('semente de aleatoriedade (deixe em branco para aleatório): ')
-    if seed == '':
-        seed = None
-    else:
-        seed = int(seed)
+def menu() -> tuple:
+    err = True
+    while err:
+        try:
+            rows = int(input('linhas: '))
+            if rows <= 0:
+                raise ValueError()
+            err = False
+        except ValueError:
+            print("Somente valores inteiros maiores que 0 para a quantidade de linhas!")
+    err = True
+    while err:
+        try:
+            columns = int(input('colunas: '))
+            if columns <= 0:
+                raise ValueError()
+            err = False
+        except ValueError:
+            print("Somente valores inteiros maiores que 0 para a quantidade de colunas!")
+        err = True
+        while err:
+            try:
+                seed = input('semente de aleatoriedade (deixe em branco para aleatório): ')
+                if seed == '':
+                    seed = None
+                else:
+                    seed = int(seed)
+                err = False
+            except ValueError:
+                print("Somente valores numéricos ou nada!")
+    return (rows, columns, seed)
 
+
+def main():
+    rows, columns, seed = menu()
+    
     m = Maze(rows, columns, seed=seed)
     
     mi = maze_to_image(m)
